c2896f1cd160f2577d836e8d928819719dd809af
Birat Rai <biratrai@hotmail.com>
2018-10-18 19:50:54 -0400

    adding ChangeBookShelf component

---
+++ src/ChangeBookShelf.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ChangeBookShelf extends Component {
+    static propTypes = {
+        book: PropTypes.object.isRequired,
+        booksList: PropTypes.array.isRequired,
+        changeBookShelf: PropTypes.func.isRequired
+    };
+
+    updateShelf = event => {
+        this.props.changeBookShelf(this.props.book, event.target.value);
+    }
+  
+    getShelfName = (booksList, book) => {
+        for (let item of booksList) {
+            if (item.id === book.id) {
+                return item.shelf;
+            }
+        }
+    }
+
+    render() {
+        const { book, booksList } = this.props;
+        
+        let currentShelf = this.getShelfName(booksList, book);
+
+        return (
+            <div className="book-shelf-changer">
+                <select onChange={ this.updateShelf } defaultValue={ currentShelf }>
+                <option value="none" disabled>
+                    Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+                </select>
+            </div>
+        );
+    }
+}
+
+export default ChangeBookShelf;

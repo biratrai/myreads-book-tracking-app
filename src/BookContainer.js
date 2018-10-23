b1a66acc1a3aac672c961544441bd0a37ed201ea
Birat Rai <biratrai@hotmail.com>
2018-10-23 15:35:44 -0400

    making it a stateless functional component since it doesn't have a state

--- src/BookContainer.js
+++ src/BookContainer.js
@@ -2,38 +2,33 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import BookShelf from './BookShelf';
 
-class BookContainer extends Component {
-  static propTypes = {
-    booksList: PropTypes.array.isRequired,
-    changeBookShelf: PropTypes.func.isRequired
-  };
+// Stateless functional Component
+const BookContainer = ({booksList, changeBookShelf}) => {
+  const shelfList = [
+    { shelfType: 'currentlyReading', shelfName: 'Currently Reading' },
+    { shelfType: 'wantToRead', shelfName: 'Want to Read' },
+    { shelfType: 'read', shelfName: 'Read' }
+  ];
 
-  state = { shelfChange: false };
-
-  render() {
-    const { booksList, changeBookShelf } = this.props;
-    const shelfList = [
-      { shelfType: 'currentlyReading', shelfName: 'Currently Reading' },
-      { shelfType: 'wantToRead', shelfName: 'Want to Read' },
-      { shelfType: 'read', shelfName: 'Read' }
-    ];
-
-    return (
-      <div className="list-books-content">
-        { shelfList.map((shelf, index) => {
-          const shelfBooks = booksList.filter(book => book.shelf === shelf.shelfType);
-          return (
-            <div className="bookshelf" key={ index }>
-              <h2 className="bookshelf-title">{ shelf.shelfName }</h2>
-              <div className="bookshelf-books">
-                <BookShelf booksList={ shelfBooks } changeBookShelf={ changeBookShelf } />
-              </div>
+  return (
+    <div className="list-books-content">
+      { shelfList.map((shelf, index) => {
+        const shelfBooks = booksList.filter(book => book.shelf === shelf.shelfType);
+        return (
+          <div className="bookshelf" key={ index }>
+            <h2 className="bookshelf-title">{ shelf.shelfName }</h2>
+            <div className="bookshelf-books">
+              <BookShelf booksList={ shelfBooks } changeBookShelf={ changeBookShelf } />
             </div>
-          );
-        })}
-      </div>
-    );
-  }
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
+BookContainer.propTypes = {
+  booksList: PropTypes.array.isRequired,
+  changeBookShelf: PropTypes.func.isRequired
+}
 export default BookContainer;
